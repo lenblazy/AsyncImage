18ce828f4f818df281a22d0738969758bda71007
lenblazy <lenblazy@gmail.com>
2023-05-23 17:11:34 +0300

    Add phases

--- AsyncImage/ContentView.swift
+++ AsyncImage/ContentView.swift
@@ -7,15 +7,53 @@
 
 import SwiftUI
 
+extension Image{
+    func imageModifier() -> some View{
+        self
+            .resizable()
+            .scaledToFit()
+    }
+    
+    func iconModifier() -> some View{
+        self
+            .imageModifier()
+            .frame(maxWidth: 128)
+            .foregroundColor(.purple)
+            .opacity(0.5)
+    }
+    
+}
+
 struct ContentView: View {
+    
+    private let imageURL: String = "https://credo.academy/credo-academy@3x.png"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+//        AsyncImage(url: URL(string: imageURL)){ image in
+//            image
+//                .imageModifier()
+//
+//        } placeholder: {
+//            Image(systemName: "photo.circle.fill")
+//                .iconModifier()
+//        }
+//        .padding(40)
+        
+        //MARK: - 4. PHASE
+        AsyncImage(url: URL(string: imageURL)){ phase in
+            
+            if let image = phase.image{
+                image.imageModifier()
+            }else if phase.error != nil{
+                Image(systemName: "ant.circle.fill")
+                              .iconModifier()
+            }else{
+                Image(systemName: "photo.circle.fill")
+                              .iconModifier()
+            }
         }
-        .padding()
+        .padding(40)
     }
 }
 
