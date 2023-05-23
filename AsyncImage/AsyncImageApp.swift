37fe61e86dc5784f53e9db15a12c19abc8f9614a
lenblazy <lenblazy@gmail.com>
2023-05-23 16:34:29 +0300

    Initial Commit

---
+++ AsyncImage/AsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsyncImageApp.swift
+//  AsyncImage
+//
+//  Created by Lennox Mwabonje on 23/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct AsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
